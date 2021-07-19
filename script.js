769e91c2505c154ca44c0f8214a407a98c311552
benig <cbhdchui@gmail.com>
2021-07-19 21:27:51 +0300

    19/07/21

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+const Authorization_Url = "https://test.api.amadeus.com/v1/security/oauth2/token";
+const HotelApiBaseUrl = "https://test.api.amadeus.com";
+const ImagesApiBaseUrl = "https://source.unsplash.com/800x450/?hotel&id=";
+const API_Keys = {
+    "grant_type": 'client_credentials',
+    "client_id": '6NvSjIcGLPFLt7eW4IGHmdJuV2AUfY8O',
+    "client_secret": 'AI5zIvWHnSvdXBcO'
+};
+
 const ELEMENTS = {
     $FROM_EL: $('#orderForm'),
     $DIV_CONTAINER: $('#result'),
     $ORDER: $('#contact_form'),
 }
-
+let HOTELS = []
 const order = [];
 
 start();
@@ -13,7 +22,7 @@ start();
 //#region Initial Loading Methods
 
 function start() {
-    
+    getAccessToken();
     getJsonFile()
     setMinimumTime('checkIn', new Date());
     validationMinimumRange();
@@ -23,7 +32,7 @@ function start() {
 }
 
 function getJsonFile() {
-    $.getJSON("test.json", function (data) {
+    $.getJSON("./assets/jsons/cities.json", function (data) {
         Object.entries(data).forEach(n => creteListCitys(n));
     }).fail(function () {
         console.log("An error has occurred.");
@@ -70,17 +79,26 @@ function creteListCitys(params) {
 }
 
 function creatingHotels(hotel) {
- console.log(hotel);
+    // const hotelImage = hotelImage; //imgs('hotel');
+    // const hotelImage = 'https://images.unsplash.com/photo-1513694203232-719a280e022f?ixid=MnwyNDYyMTN8MHwxfHNlYXJjaHwxfHxyb29tfGVufDB8fHx8MTYyNjY0MTI4Mw&ixlib=rb-1.2.1'
+
+
     ELEMENTS.$DIV_CONTAINER.append(`
      
     <div class="HotelDetails d-flex flex-row">
     <div class="foto p2">
-        <img src=${imgs('hotel')} alt="" class="hotelImg">
+        <img src="assets/images/237729537.jpg" alt="" class="hotelImg">
     </div>
     <div class="text p2">
 
         <div class="bodyOfContent">
+            <div>
             <h1>${hotel.nema}</h1>
+            </div>
+            <div>
+            <a href="https://www.google.com/maps/place/${hotel.city}${hotel.address}" target="_blank">${hotel.city}${hotel.address}</a>
+
+            </div>
             <div>
             ${creatStars(hotel.rating)}
             </div>
@@ -88,26 +106,27 @@ function creatingHotels(hotel) {
         </div>
 
         <div class="endOfContent">
-            <p>${hotel.description_hotel}</p>
+            ${hotel.description_hotel}
         </div>
         <div class="ContentHead d-flex flex-row">
             <div class="p2">
                 <p>${hotel.price1},${hotel.price2}</p>
-                <p>readily available
-                    Not available
-                    ${hotel.description_room}</p>
+                <p> ${hotel.description_room}</p>
             </div>
+            <div>
+  <select class="custom-select"></select>
+  ${hotel.options}
+  </select>
+  </div>
             <div class="p2 btnRoom">
                 <button type="submit" class="roomOrder btn btn-success" onclick = CreatOrder()>order</button>
-              
-
             </div>
 
         </div>
 
     </div>
 
-</div>    ` ) 
+    </div>    ` )
     // אולי צריך להחזיר לכפתור את (id="myBtn")
 }
 
@@ -127,16 +146,17 @@ function creatStars(number) {
 
 function addEvent() {
     ELEMENTS.$FROM_EL.on("submit", function (event) {
+        HOTELS = []
+        ELEMENTS.$DIV_CONTAINER.empty();
         event.preventDefault();
         $(".roaderSurface").css("display", "block");
-        getTokens();
         const form = event.target
         const data = {
             city: form.city.value,
             checkIn: form.checkIn.value,
             checkOut: form.checkOut.value,
         }
-        getHotelsJsonFile()
+        findHotelsOffers(form.city.value, form.checkIn.value, form.checkOut.value);
         form.reset();
 
         console.log(data);
@@ -144,7 +164,7 @@ function addEvent() {
 }
 
 function setMinimumTime(id, date) {
-    document.getElementById(id).setAttribute("min", changeDateFormat(date));
+    $(`#${id}`).attr("min", changeDateFormat(date))
 }
 
 function changeButActive() {
@@ -159,11 +179,6 @@ function CreatingHotel(hotel) {
 }
 
 //#endregion DOM Manipulation Methods
-
-
-//#region Local Storage Methods
-
-//#endregion
 
 
 //#region Logical  Methods
@@ -183,63 +198,92 @@ function padLeadingZero(number) {
 //#endregion Logical  Methods
 
 
-//#region External readings
+//#region Local Storage Methods
 
-function getTokens() {
-    $.get({
-        method: "post",
-        url: 'https://test.api.amadeus.com/v1/security/oauth2/token',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        data: "grant_type=client_credentials&client_id=6NvSjIcGLPFLt7eW4IGHmdJuV2AUfY8O&client_secret=AI5zIvWHnSvdXBcO",
-        success: function (result) {
-            if (result) {
-                console.log(result);
-            }
-        }
-    });
-
+function setApiBearer(accessToken) {
+    localStorage.setItem('ApiBearer', accessToken);
 }
 
-function imgs(type) {
-    const url = `https://api.unsplash.com/search/photos/?query=${type}&page=1&per_page=1&client_id=fxxTEmUpHWL_AY16kZr7CDmk7YQvpZ0hmgnF6kl13Kk`
-    $.get(url, (data) => {
-        if (data) {
-            const imgUrl = data.results[0].urls.full;
-            return imgUrl
-        }
-    })
+function getApiBearer() {
+    return localStorage.getItem('ApiBearer');
 }
 
 //#endregion
 
 
+//#region External api readings
 
-const modal = document.getElementById("myModal");
+function joinObjectKeysAndValues(objectToJoin) {
+    const glue = '=',
+        separator = '&';
+    return Object.entries(objectToJoin).map((key, value) => key.join(glue)).join(separator);
+}
 
-const btn = document.getElementById("myBtn");
+function getAccessToken() {
+    $.get({
+        method: "post",
+        url: Authorization_Url,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: joinObjectKeysAndValues(API_Keys),
+        success: (result) => {
+            if (result) {
+                // console.log(result);
+                const {
+                    access_token, expires_in
+                } = result;
+                // console.log({ access_token, expires_in });
+                setApiBearer(access_token);
+            }
+        }
+    });
+}
 
-const span = document.getElementsByClassName("close")[0];
+function imgs(type) {
+    const url = `https://api.unsplash.com/search/photos/?query=${type}&page=1&per_page=1&client_id=fxxTEmUpHWL_AY16kZr7CDmk7YQvpZ0hmgnF6kl13Kk`;
+    var imgUrl;
+    $.get(url, (data) => {
+        // console.log(data);
+        if (data) {
+            imgUrl = data.results[0].urls.full;
+            // console.log(imgUrl);
+            return imgUrl;
+        }
+    });
+}
+
+function findHotelsOffers(cityCode, checkInDate, checkOutDate) {
+    const reqParams = {
+        cityCode,
+        checkInDate,
+        checkOutDate,
+    };
+    // console.log(`${HotelApiBaseUrl}/v2/shopping/hotel-offers?${joinObjectKeysAndValues(reqParams)}`);
+    $.get({
+        method: "GET",
+        url: `${HotelApiBaseUrl}/v2/shopping/hotel-offers?${joinObjectKeysAndValues(reqParams)}`,
+        headers: {
+            'Authorization': `Bearer ${getApiBearer()}`
+        },
+        success: (result) => {
+            if (result.data.length) {
+                $(".roaderSurface").css("display", "none");
+
+                console.log({
+                    result
+                });
+                // const areAvailableHotels = result.data;
+                Object.entries(result.data).forEach(n => hotelObjet(n));
+                HOTELS.forEach(creatingHotels);
+            }
+        }
+    });
+}
+
+//#endregion
 
 
-
-
-
-// function getTokens2() {
-//     $.get({
-//         method: "post",
-//         url: 'https://test.api.amadeus.com/v1/security/oauth2/token',
-//         headers: { "Authorization: Bearer B1uK7IaUyN44sFc2xm8oYUvPFcJa" },
-//         data: "Authorization: Bearer fZOO7SZ6YZkuGjFxgNeUrRD2VQjx",
-//         success: function (result) {
-//             if (result) {
-//                 console.log(result);
-//                 $(".roaderSurface").hide;
-
-//             }
-//         }
-//     });
-
-// }
 
 
 // function validateProprietyForm() {
@@ -263,8 +307,15 @@ const span = document.getElementsByClassName("close")[0];
 
 
 // ---------------- פונקציות לא גמורות -------------
+
+
+const modal = $("#myModal");
+
+const btn = $(".close");
+
+
 function CreatOrder(x) {
-    modal.style.display = "block";
+    modal.css('display', "block")
     ELEMENTS.$ORDER.on("submit", function (event) {
         event.preventDefault();
         const form = event.target
@@ -274,24 +325,34 @@ function CreatOrder(x) {
 
 
     });
+}
+btn.on('click', () => {
+    modal.css('display', "none");
 
+    saveToLocalStorage(key, value);
 
+})
 
+$(window).on('click', function (event) {
+    if (event.target == modal) {
+        console.log('yesss');
+        modal.css('display', "none");
 
-    span.onclick = function () {
-        modal.style.display = "none";
         saveToLocalStorage(key, value)
     }
-
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-            saveToLocalStorage(key, value)
-        }
-    }
+})
 
 
-}
+
+// window.onclick = function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//         saveToLocalStorage(key, value)
+//     }
+// }
+
+
+
 
 function saveToLocalStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
@@ -299,38 +360,56 @@ function saveToLocalStorage(key, value) {
 
 
 
-const HOTELS = []
-
-function getHotelsJsonFile() {
 
 
-    $.getJSON("hotel.json", function (hotels) {
-        Object.entries(hotels.data).forEach(n => hotelObjet(n));
-        HOTELS.forEach(creatingHotels);
-        $(".roaderSurface").css("display", "none");
-    }).fail(function () {
-        console.log("An error has occurred.");
-    });
-    
-   
-}
+// function getHotelsJsonFile() {
+
+
+//     $.getJSON("./assets/jsons/hotel.json", function (hotels) {
+
+//     }).fail(function () {
+//         console.log("An error has occurred.");
+//     });
+
+
+// }
 
 
 function hotelObjet(hotel) {
 
+    let description = ''
+    if (hotel[1].hotel.description) {
+        description = hotel[1].hotel.description.text
+    } else {
+        description = 'No description of selected hotel';
+    }
+
     const hotels = {
         'nema': hotel[1].hotel.hotelId,
-        'rating':  hotel[1].hotel.rating,
-        'description_hotel':  hotel[1].hotel.description.text,
+        'city': hotel[1].hotel.address.cityName,
+        'address': hotel[1].hotel.address.lines[0],
+        'rating': hotel[1].hotel.rating,
+        'description_hotel': description,
         'price1': hotel[1].offers[0].price.currency,
         'price2': hotel[1].offers[0].price.total,
-        'description_room': hotel[1].offers[0].room.description.text 
+        'options': options(hotel[1].offers[0].price.variations.changes),
+        'description_room': hotel[1].offers[0].room.description.text
     }
     HOTELS.push(hotels);
 
 }
 
-
+function options(option) {
+  let  rooms = '';
+    if (option.length > 1) {
+        for (let i = 0; i <=option.length; i++) {
+            rooms += `<option value="">startDate: "${option[i].startDat}", endDate: "${option[i].endDate}", total: "${option[i].total}"</option>`
+        }
+    } else {
+        rooms = `<option value="">startDate: "${option[0].startDat}", endDate: "${option[0].endDate}", total: "${option[0].total}"</option>`;
+    }
+    return rooms
+}
 
 
 
