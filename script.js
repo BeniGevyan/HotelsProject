05b7972bc42a54e3d33ef64364ff0873ba242d05
benig <cbhdchui@gmail.com>
2021-07-20 00:56:40 +0300

    20/07/21

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const Authorization_Url = "https://test.api.amadeus.com/v1/security/oauth2/token";
 const HotelApiBaseUrl = "https://test.api.amadeus.com";
+const bookingsUrl ='https://test.api.amadeus.com/v1/booking/hotel-bookings'
 const ImagesApiBaseUrl = "https://source.unsplash.com/800x450/?hotel&id=";
 const API_Keys = {
     "grant_type": 'client_credentials',
@@ -22,6 +23,7 @@ start();
 //#region Initial Loading Methods
 
 function start() {
+    // addevent()
     getAccessToken();
     getJsonFile()
     setMinimumTime('checkIn', new Date());
@@ -85,48 +87,55 @@ function creatingHotels(hotel) {
 
     ELEMENTS.$DIV_CONTAINER.append(`
      
-    <div class="HotelDetails d-flex flex-row">
-    <div class="foto p2">
-        <img src="assets/images/237729537.jpg" alt="" class="hotelImg">
-    </div>
-    <div class="text p2">
 
-        <div class="bodyOfContent">
-            <div>
-            <h1>${hotel.nema}</h1>
-            </div>
-            <div>
-            <a href="https://www.google.com/maps/place/${hotel.city}${hotel.address}" target="_blank">${hotel.city}${hotel.address}</a>
-
-            </div>
-            <div>
-            ${creatStars(hotel.rating)}
-            </div>
-                  
-        </div>
-
-        <div class="endOfContent">
-            ${hotel.description_hotel}
-        </div>
-        <div class="ContentHead d-flex flex-row">
-            <div class="p2">
-                <p>${hotel.price1},${hotel.price2}</p>
-                <p> ${hotel.description_room}</p>
-            </div>
-            <div>
-  <select class="custom-select"></select>
-  ${hotel.options}
-  </select>
+  <div class="HotelDetails d-flex flex-row">
+  <div class="foto p2">
+    <img src="assets/images/237729537.jpg" alt="" class="hotelImg">
   </div>
-            <div class="p2 btnRoom">
-                <button type="submit" class="roomOrder btn btn-success" onclick = CreatOrder()>order</button>
-            </div>
+  <div class="text p2">
 
-        </div>
+    <div class="bodyOfContent">
+      <div>
+        <h1>${hotel.nema}</h1>
+      </div>
+      <div>
+        <a href="https://www.google.com/maps/place/${hotel.city}${hotel.address}"
+          target="_blank">${hotel.city}${hotel.address}</a>
+
+      </div>
+      <div>
+        ${creatStars(hotel.rating)}
+      </div>
 
     </div>
 
-    </div>    ` )
+    <div class="endOfContent">
+      ${hotel.description_hotel}
+    </div>
+    <div class="ContentHead d-flex flex-column">
+      <div class="p2">
+        <p>${hotel.price1},${hotel.price2}</p>
+        <p> ${hotel.description_room}</p>
+      </div>
+      <div class="p2 d-flex flex-row">
+        <div>
+        
+          <select class="p2">
+          ${hotel.Default}
+            ${hotel.options}
+          </select>
+        </div>
+        <div class="btnRoom p2">
+          <button type="submit" class="roomOrder btn btn-success" onclick=CreatOrder()>order</button>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+` )
     // אולי צריך להחזיר לכפתור את (id="myBtn")
 }
 
@@ -149,6 +158,7 @@ function addEvent() {
         HOTELS = []
         ELEMENTS.$DIV_CONTAINER.empty();
         event.preventDefault();
+        $("#loaderModal").css("display", "block");
         $(".roaderSurface").css("display", "block");
         const form = event.target
         const data = {
@@ -266,18 +276,20 @@ function findHotelsOffers(cityCode, checkInDate, checkOutDate) {
         headers: {
             'Authorization': `Bearer ${getApiBearer()}`
         },
+        complete: () => {
+            $("#loaderModal").css("display", "none");
+            $(".roaderSurface").css("display", "none");
+        },
         success: (result) => {
             if (result.data.length) {
-                $(".roaderSurface").css("display", "none");
-
-                console.log({
-                    result
-                });
-                // const areAvailableHotels = result.data;
                 Object.entries(result.data).forEach(n => hotelObjet(n));
                 HOTELS.forEach(creatingHotels);
             }
-        }
+        },
+        error: () => {
+
+        },
+
     });
 }
 
@@ -360,8 +372,6 @@ function saveToLocalStorage(key, value) {
 
 
 
-
-
 // function getHotelsJsonFile() {
 
 
@@ -392,27 +402,106 @@ function hotelObjet(hotel) {
         'description_hotel': description,
         'price1': hotel[1].offers[0].price.currency,
         'price2': hotel[1].offers[0].price.total,
+        'Default': Default(hotel[1].offers[0]),
         'options': options(hotel[1].offers[0].price.variations.changes),
         'description_room': hotel[1].offers[0].room.description.text
     }
     HOTELS.push(hotels);
 
 }
-
+function Default(params) {
+    return `<option value="" selected >startDate: "${params.checkInDate}", endDate: "${params.checkOutDate}", total: "${params.price.total}"</option>`
+}
 function options(option) {
-  let  rooms = '';
+    let rooms = '';
     if (option.length > 1) {
-        for (let i = 0; i <=option.length; i++) {
-            rooms += `<option value="">startDate: "${option[i].startDat}", endDate: "${option[i].endDate}", total: "${option[i].total}"</option>`
+        for (let i = 0; i < option.length; i++) {
+            rooms += `<option value="">startDate: "${option[i].startDate}", endDate: "${option[i].endDate}", total: "${option[i].total}"</option>`
         }
     } else {
-        rooms = `<option value="">startDate: "${option[0].startDat}", endDate: "${option[0].endDate}", total: "${option[0].total}"</option>`;
+        rooms = `<option value="">startDate: "${option[0].startDate}", endDate: "${option[0].endDate}", total: "${option[0].total}"</option>`;
     }
     return rooms
 }
 
+// ---------------------------/
 
 
 
+function addevent() {
+    $('#formcontainer').on("submit", function (event) {
+        console.log('1111111');
+
+        event.preventDefault();
+        const form = event.target
+        const data = {
+            first_name: form.first_name.value,
+            last_name: form.last_name.value,
+            email: form.email.value,
+            contact_no: form.contact_no.value,
+        }
+        validData(data) 
+
+    });
+}
+
+function validData(data) {
+    if (ValidaName(data.first_name) && ValidaName(data.last_name) && ValidateEmail(data.email) &&  phonenumber(data.contact_no)) {
+        $('#formcontainer').addClass('was-validated')
+
+    }
+ 
+}
+function ValidaName(name) {
+    const clinSpace = name.trim();
+    if (!clinSpace) {
+        return false;
+    } else {
+
+        return true;
+    }
+
+}
+
+function ValidateEmail(mail) {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail.value)) {
+        return (true)
+    } else {
+
+        return false;
+    }
 
 
+}
+function phonenumber(inputphon) {
+    const phone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if ((inputphon.value.match(phone))) {
+        return true;
+    } else {
+
+        return false;
+    }
+}
+
+
+
+$.post({
+    method: "POST",
+    url: `${HotelApiBaseUrl}/v2/shopping/hotel-offers?${joinObjectKeysAndValues(reqParams)}`,
+    headers: {
+        'Authorization': `Bearer ${getApiBearer()}`
+    }
+    
+    
+    ,
+    complete: () => {
+      
+    },
+    success: (result) => {
+    
+    },
+    error: () => {
+
+    },
+
+});
